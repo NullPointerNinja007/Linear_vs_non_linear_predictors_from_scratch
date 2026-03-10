d6690316b3c53cd7450ea29f1e36e37dc355d4e1
NullPointerNinja007 <raddaigalamaleeka@gmail.com>
2026-03-09 22:26:31 -0700

    minor changes

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,29 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# The original dataset has 5 classes (0–4) representing disease severity.
-# To make multiclass classification more stable with this small dataset,
-# we merge classes 2, 3, and 4 into a single category.
-# Final labels become: 0 (none), 1 (mild), 2 (moderate/severe)
 
-#data preprocessing steps (uncomment to run)
-
-#path = 'data/heart+disease/processed.cleveland.data' this  is the original data path
-#data[13] = data[13].replace({2:2, 3:2, 4:2}) #code to process the data by merging classes 2, 3, and 4 into a single category (2)
-#data.to_csv("data/heart_processed.csv", index=False) this is the code to save the processed data to a new csv file
-#data = data.replace("?", pd.NA)
-#data = data.dropna()
-#data = data.astype(float)
-#data.to_csv("data/heart_processed.csv", index=False)
-
-path = 'data/heart_processed.csv'  # This is the path to the processed data
-data = pd.read_csv(path, header=None)
-print(data.head())
-#print(data.shape)
-#print(data[13].value_counts())
-#print(data.isna().sum())
-print(data.isna().sum())
-print(data.shape)
 
 def calculate_gradient(X, y, theta):
 
